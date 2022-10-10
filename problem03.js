028b401b5a509a0dc5410913b6d0280f02a9b103
Avishek Singh <simisingh12102002@gmail.com>
2022-10-10 18:44:17 +0530

    Day 1 Program

---
+++ problem03.js
@@ -0,0 +1,4 @@
+let name = "Avishek Kumar Singh";
+let age = 22;
+console.log(name,age);
+console.log(typeof(name),typeof(age));
---
+++ problem04.js
@@ -0,0 +1,29 @@
+let sch = "D.A.V PUBLIC SCHOOL";
+console.log(sch);
+console.log(" ")
+
+let n = "Avishek Singh";
+console.log(n);
+
+let r = "Roll No";
+console.log(r + " - " + 14);
+
+let s = "Section";
+console.log(s + " - " + "A");
+console.log(" ")
+
+let eng = "ENGLISH COMMUNICATION";
+console.log(eng + "                 " + "- " + 99);
+
+let cs = "COMPUTER SCIENCE";
+console.log(cs + "                      " + "- " + 95);
+
+let d = "DATA STRUCTURES & ALGORITHMS";
+console.log(d + "          " + "- " + 92);
+console.log(" ")
+
+let tm = "Total Marks";
+console.log(tm + " - " + 286);
+
+let g = "Grade";
+console.log(g + " - " + "A");
